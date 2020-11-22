1e794153344c4c95fa85726cb9aed8843d4d5b6f
pashentiy <pashentiy.90@gmail.com>
2020-11-22 20:13:10 +0200

    Map View integrated

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 
 const HomeScreen = () => {
     return (
         <View style={s.container}>
-            <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems:'center' }}>
-              <Text style={{ }}>Google Map</Text>
-            </ScrollView>
+            {/* <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems:'center' }}> */}
+            <MapView
+                style={s.container}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+            >
+            </MapView>
+            <Text style={{}}>Google Map</Text>
+            {/* </ScrollView> */}
         </View >
     );
 }
@@ -21,6 +32,9 @@ const s = StyleSheet.create({
         width: '100%',
         alignSelf: 'center',
 
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
     },
     balanceView: {
         marginVertical: 20
