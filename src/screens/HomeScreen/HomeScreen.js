3053c1b9e73d86d7c7894a4aacb0f4383fd65604
pashentiy <pashentiy.90@gmail.com>
2020-11-24 15:24:48 +0200

    Google Maps Integretion completed

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,42 +1,46 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 
 const HomeScreen = () => {
-    return (
-        <View style={s.container}>
-            {/* <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems:'center' }}> */}
-            <MapView
-                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-                style={s.container}
-                region={{
-                    latitude: -33.86,
-                    longitude: 151.20,
-                }}
-            />
-            <Text style={{}}>Google Map</Text>
-            {/* </ScrollView> */}
-        </View >
-    );
+  return (
+    // <View style={s.container}>
+      // {/* <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems:'center' }}> */}
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={{ flex: 1 }}
+        region={{
+          latitude: 31.750139,
+          longitude: 35.2372,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.0025,
+        }}>
+
+      </MapView>
+      // <Text style={{}}>Google Map</Text>
+      // {/* </ScrollView> */}
+    // </View >
+  );
 }
 
 export default HomeScreen;
 
 
 const s = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        alignSelf: 'center',
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-    },
-    balanceView: {
-        marginVertical: 20
-    },
-    verificationView: {
-        marginTop: 20
-    }
+  container: {
+    flex: 1,
+    width: '100%',
+    alignSelf: 'center',
+
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  balanceView: {
+    marginVertical: 20
+  },
+  verificationView: {
+    marginTop: 20
+  }
 })
