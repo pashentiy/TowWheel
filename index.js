1300e4ca5b34ae17841b291c4f9c8ff7c1ac651b
pashentiy <pashentiy.90@gmail.com>
2020-12-30 02:32:08 +0200

    added amplify and pushed into cloud,mis aws-export

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /**
  * @format
  */
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
-import App from './src/App';
-import {name as appName} from './app.json';
+import App from './src';
+import { name as appName } from './app.json';
+
 
 
 AppRegistry.registerComponent(appName, () => App);
