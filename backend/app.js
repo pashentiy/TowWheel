4d6e4366442a2255f2ecc3c797fc471625dfd9f5
pashentiy <pashentiy.90@gmail.com>
2021-03-16 23:40:50 +0200

    added body parser and connect to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const app = express();
-const port = process.env.port || "3001";
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const fileupload = require("express-fileupload");
 
-app.get("/", (req, res) => {
-    res.send("OK");
-} )
-app.listen(port, () => console.log('listening on port ' + port));
+
+
+//Init DB Connection
+mongoose.connect("", { auth:{authdb:"admin"}, useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true, useCreateIndex: true, useFindAndModify: false })
+.then((e) => console.log('DB Connected.'))
+.catch(error => { console.log(error) });
+
+//Init app server
+const app = express()
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit:50000 }));
+app.use(bodyParser.raw({ limit: '50mb'}) );
+app.use(fileupload({
+	limits: { fileSize: 50 * 1024 * 1024 },
+	debug: false,
+}));
+app.use(express.static('public'))
+
