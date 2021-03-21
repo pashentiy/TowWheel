1123b0ab2b191619af30d9aec859f1c595d6bccd
pashentiy <pashentiy.90@gmail.com>
2021-03-21 21:24:15 +0200

    mongos added to index model

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,9 @@
+const mongoose = require('mongoose');
+
 exports.Otp = require('./OtpModel');
 exports.User = require('./UserModel');
 exports.Driver = require('./DriverModel');
 exports.Vehicle = require('./VehicleModel');
 exports.Ride = require('./RideModel');
 exports.Review = require('./ReviewModel');
+exports.Mongoose = mongoose;
