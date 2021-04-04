0fc987b318d9da79e36b2b4d50472be54b9b1071
pashentiy <pashentiy.90@gmail.com>
2021-04-04 21:32:28 +0300

    added config type setting

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,18 @@
 import ENVDATA from "../../env.json"
 
+import {
+    Platform
+} from 'react-native';
+
+var config = {
+    isAndroid: Platform.OS === "android" ? true : false,
+    isIos: Platform.OS === "android" ? false : true,
+    android_version: '1.0.0',
+    ios_version: '1.0.0',
+	online_session_timeout: 3, // in Minute
+	session: null
+}
 export default config
+export const BASE_WIDTH = 360
+export const BASE_HEIGHT = 640
 
