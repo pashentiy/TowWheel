42dc9647c6d4b643608ef68607238e41a8863e63
pashentiy <pashentiy.90@gmail.com>
2021-04-04 21:33:55 +0300

    added additional config settings for multi language

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -15,4 +15,13 @@ var config = {
 export default config
 export const BASE_WIDTH = 360
 export const BASE_HEIGHT = 640
-
+export const API_URL = ENVDATA.API_URL
+export const API_STORAGE = ENVDATA.API_STORAGE
+export const GOOGLE_MAP_API_KEY = ENVDATA.GOOGLE_MAP_API_KEY
+export const LOCALES = {
+	ENGLISH: { id: 1, name: "en", label: "ENGLISH" },
+	HEBREW: { id: 2, name: "he", label: "HEBREW" }
+};
+export const DOMAIN = ''
+export const APP_URL = '' 
+export const ENVIRONMENT = ENVDATA.ENVIRONMENT
