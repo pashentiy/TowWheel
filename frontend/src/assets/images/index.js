20e9eb6bf04cbd4915c231fd2f54abd769654a57
pashentiy <pashentiy.90@gmail.com>
2021-04-04 20:48:35 +0300

    added new assets

---
+++ frontend/src/assets/images/index.js
@@ -0,0 +1,5 @@
+export const logo = require('src/assets/images/logo.png') 
+export const hamburger = require('src/assets/images/hamburger.png') 
+export const login = require('src/assets/images/login.png') 
+export const verification = require('src/assets/images/verification.png') 
+export const handset = require('src/assets/images/handset.png') 
