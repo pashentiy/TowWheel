75292e4262917bfaf7aa662281af935918ae14be
shmuelma <shmuel.m800@gmail.com>
2021-04-07 16:36:34 +0300

    import

---
+++ frontend/src/hooks/useStorage/index.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+/*
+ * Hook for asyncstorage
+ * const [theme, changeTheme, clearAll] = useStorage("@themeIsDark", false);
+ * changeTheme(null) - to remove item
+ */
