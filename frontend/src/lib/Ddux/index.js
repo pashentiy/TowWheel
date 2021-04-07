64704f6d5b47bc8851e9c3d758e6c0b70edf8dd1
shmuelma <shmuel.m800@gmail.com>
2021-04-07 18:43:44 +0300

    Ddux

--- frontend/src/lib/Ddux/index.js
+++ frontend/src/lib/Ddux/index.js
@@ -8,3 +8,44 @@ export const DduxContext = React.createContext({
     setData: () => { },
     setCache: () => { }
 });
+
+
+export const DduxProvider = ({ initialState = {}, initialCache = {}, ...props }) => {
+
+    const cacheKey = "@dduxCache"
+    const [cacheData, setCacheData] = useState({})
+    const [data, setData] = useState(initialState)
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    const init = async () => {
+        const cache = JSON.parse(await AsyncStorage.getItem(cacheKey))
+        if (cache)
+            setCacheData(cache)
+        else
+            setCacheData(initialCache)
+    }
+
+    const setCache = async (newValue) => {
+        try {
+            const value = JSON.stringify(newValue);
+            await AsyncStorage.setItem(cacheKey, value);
+        } catch (e) { }
+    }
+
+    const _setData = useCallback((key, value) => {
+        setData(prevState => {
+            return { ...prevState, [key]: value }
+        });
+    }, []);
+
+
+    const _getData = (key) => {
+        try {
+            return data[key]
+        } catch (e) {
+            console.log(e)
+        }
+    }
