97a3db567d9d2025eb2dbb2e4f87fb1b79021084
shmuelma <shmuel.m800@gmail.com>
2021-04-07 16:56:39 +0300

    exports

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useStorage } from './useStorage'
 export { default as useLanguage } from './useLanguage'
 export { default as useTheme } from './useTheme'
+export { default as useDdux } from './useDdux'
