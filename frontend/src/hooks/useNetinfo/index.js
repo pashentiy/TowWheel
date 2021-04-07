b8c9e5808265a2acd67e6ee075a00a131ed8846f
shmuelma <shmuel.m800@gmail.com>
2021-04-07 15:20:29 +0300

    funcstate

--- frontend/src/hooks/useNetinfo/index.js
+++ frontend/src/hooks/useNetinfo/index.js
@@ -6,3 +6,9 @@ import NetInfo from "@react-native-community/netinfo";
  * const netinfo = useNetinfo();
  * 
  */
+
+const inititalState = {
+    type: null,
+    isInternetReachable: null,
+    isConnected: null
+  };
