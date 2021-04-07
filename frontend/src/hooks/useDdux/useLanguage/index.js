badf00420f094427a2942bfa003a9be7cc6d9494
shmuelma <shmuel.m800@gmail.com>
2021-04-07 15:13:20 +0300

    func use

--- frontend/src/hooks/useDdux/useLanguage/index.js
+++ frontend/src/hooks/useDdux/useLanguage/index.js
@@ -1,2 +1,12 @@
 import React, { useState, useEffect, useContext, useMemo } from "react";
 import { LanguageContext } from 'src/lib'
+
+/*
+ * Hook for Theme and Language setup
+ */
+const useLanguage = () => {
+    return useContext(LanguageContext);
+  };
+  
+  
+  export default useLanguage
