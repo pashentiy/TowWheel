9583e49db477b27103ab1faf7b354c5b851259db
shmuelma <shmuel.m800@gmail.com>
2021-04-07 16:50:23 +0300

    usetheme

---
+++ frontend/src/hooks/useTheme/index.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect, useContext, useMemo } from "react";
+import { ThemeContext } from 'src/lib'
+import { Styles } from 'src/styles'
+/*
+ * Hook for Theme and Language setup
+ */
