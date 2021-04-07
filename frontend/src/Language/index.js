32a7bcfc9386013d6d8646980ec8f314af86711c
shmuelma <shmuel.m800@gmail.com>
2021-04-07 18:29:04 +0300

    languagefunc

---
+++ frontend/src/Language/index.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import {
+    I18nManager
+} from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { LOCALES } from 'src/config';
+import I18n from 'react-native-i18n';
+
+//default locale
+I18n.defaultLocale = LOCALES.ENGLISH.name;
+
+// Enable fallbacks  to `en`
+I18n.fallbacks = true;
+
+//current locale
+I18n.locale = LOCALES.ENGLISH.name;
+
+//Add new language files here
+I18n.translations = {
+    en: require("src/utils/languages/en.json"),
+    he: require("src/utils/languages/he.json")
+};
