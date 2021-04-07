5a0e1cb6b6a80418628f0e7bc194e15f81148073
shmuelma <shmuel.m800@gmail.com>
2021-04-07 16:53:09 +0300

    usetheme

--- frontend/src/hooks/useTheme/index.js
+++ frontend/src/hooks/useTheme/index.js
@@ -5,21 +5,23 @@ import { Styles } from 'src/styles'
  * Hook for Theme and Language setup
  */
 const useTheme = (style={}) => {
-    const {colors, theme, changeTheme, themeKeys} = useContext(ThemeContext);
-    const replaceWithColor = (match, capture) => {
-      return colors ? colors[capture]+'\"' : '#FFF'+'\"'
-    }
-}
-const parsedCommonStyle = JSON.parse(JSON.stringify(Styles).replace(/Colors.(.*?)\"/gi,replaceWithColor))
-let styles = {}
-if(typeof style == 'function'){
-  const parsedStyle = style({Colors:colors})
+  const {colors, theme, changeTheme, themeKeys} = useContext(ThemeContext);
+  const replaceWithColor = (match, capture) => {
+    return colors ? colors[capture]+'\"' : '#FFF'+'\"'
+  }
+  const parsedCommonStyle = JSON.parse(JSON.stringify(Styles).replace(/Colors.(.*?)\"/gi,replaceWithColor))
+  let styles = {}
+  if(typeof style == 'function'){
+    const parsedStyle = style({Colors:colors})
+    styles = useMemo(() => ({...parsedCommonStyle,...parsedStyle}), [theme]);
+  }
+  else{
+  const parsedStyle = JSON.parse(JSON.stringify(style).replace(/Colors.(.*?)\"/gi,replaceWithColor))
   styles = useMemo(() => ({...parsedCommonStyle,...parsedStyle}), [theme]);
-}
-else{
-const parsedStyle = JSON.parse(JSON.stringify(style).replace(/Colors.(.*?)\"/gi,replaceWithColor))
-styles = useMemo(() => ({...parsedCommonStyle,...parsedStyle}), [theme]);
-}
+  }
 
-return [colors,styles,theme,changeTheme,themeKeys]
+  return [colors,styles,theme,changeTheme,themeKeys]
 };
+
+
+export default useTheme
