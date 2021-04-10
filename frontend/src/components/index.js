caf24127be707692182b4428fd5a72b3fcccdf1a
pashentiy <pashentiy.90@gmail.com>
2021-04-10 18:28:48 +0300

    added export for all components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Loader} from "./Loader"
+export {default as Container} from "./Container"
+export {default as AutoResizeInputBox} from "./AutoResizeInputBox"
+export {default as KeyboardHandledView} from "./KeyboardHandledView"
+export {default as GlobalLayouts} from "./GlobalLayouts"
+export {default as InternetConnectionPopup } from "./InternetConnectionPopup"
+export {default as HeaderBar } from "./HeaderBar"
+export {default as Toast} from "./Toast"
