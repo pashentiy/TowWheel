4c9d34d2c4e21ef80031910dada67c42a5f3e905
pashentiy <pashentiy.90@gmail.com>
2021-04-10 18:28:01 +0300

    added container for login screen

---
+++ frontend/src/components/Container.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    StyleSheet
+} from 'react-native';
+import { Mixins } from 'src/styles'
+import { useTheme } from 'src/hooks'
+
+const Container = ({ isTransparentStatusBar = false, ...props }) => {
+    const [Colors, styles] = useTheme(style)
+    return (
+        <View style={[styles.container, props.style]}>
+            {isTransparentStatusBar==false && <MyStatusBar />}
+            {props.children}
+        </View>
+    );
+}
+
+const MyStatusBar = ()=>{
+    const [Colors, styles] = useTheme(style)
+    return(
+        <View style={[styles.statusBar,{height: Mixins.STATUSBAR_HEIGHT}]}>
+        </View>
+    )
+}
+
+
+const style = ({Colors})=>(StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    statusBar:{
+        backgroundColor: Colors.status_bar
+    }
+})
+);
+
+export default Container
+
+
+
