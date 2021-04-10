8bec014299103d13c9a202bed5f70a1040ea3acb
pashentiy <pashentiy.90@gmail.com>
2021-04-10 18:38:32 +0300

    change drawer logic and hierarchy

--- frontend/src/navigation/Drawer.js
+++ frontend/src/navigation/Drawer.js
@@ -1,44 +1,47 @@
-import React from 'react';
-import { createDrawerNavigator, DrawerItem, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+    Keyboard,
+    View,
+    StyleSheet
+} from 'react-native';
+import {
+    createDrawerNavigator,
+    NavigationContainer,
+    DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem
+} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import * as Screen from 'src/screens';
+import { Mixins } from 'src/styles';
+import { useTheme } from 'src/hooks'
+import CustomDrawerContent from './CustomDrawerContent'
 
-import { colors, fonts } from '../utilities';
-import Login from '../screens/LoginScreen/';
-import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen/';
-import Hamburger from './Hamburger';
-
-function CustomDrawerContent(props) {
-    return (
-        <DrawerContentScrollView {...props}>
-            <DrawerItemList {...props} />
-
-        </DrawerContentScrollView>
-    );
-}
 const Drawer = createDrawerNavigator();
 
-const DrawerContainer = () => {
+const Dashboard = () => {
+    const [Colors, styles] = useTheme(style)
     return (
         <>
-            <Drawer.Navigator
-                title={"HELLO"}
-                drawerStyle={{
-                    backgroundColor: colors.$white,
-                }}
-                drawerContent={(props) => <CustomDrawerContent {...props} />}
-                initialRouteName="Home"
-            >
-                <Drawer.Screen name="Home"
-                    options={{
-                        headerShown: null,
-                    }} component={HomeScreen} />
-                <Drawer.Screen name="Sign In"
-                    options={{
-                        headerShown: null,
-                    }} component={LoginScreen} />
-            </Drawer.Navigator>
+        <View style={[styles.statusBar, { height: Mixins.STATUSBAR_HEIGHT }]}>
+        </View>
+        <Drawer.Navigator
+            initialRouteName="Home"
+            drawerStyle={{backgroundColor: Colors.background}}
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
+            <Drawer.Screen name="Home" component={Screen.Home} />
+            <Drawer.Screen name="Notifications" component={Screen.Home} />
+        </Drawer.Navigator>
         </>
     );
 }
 
-export default DrawerContainer;
+const style = ({Colors})=>(StyleSheet.create({
+    statusBar: {
+        backgroundColor: Colors.status_bar,
+    }
+}))
+
+
+export default Dashboard
