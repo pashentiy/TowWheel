62ee2ce78e1c80885c8c580a98ef0e66c09aa28e
pashentiy <pashentiy.90@gmail.com>
2021-04-14 21:06:16 +0300

    import new assets + initial region changed to current location + added marker render for show the Tow Type on Map

--- frontend/src/screens/Home/body.js
+++ frontend/src/screens/Home/body.js
@@ -10,6 +10,7 @@ import { Mixins } from '../../styles'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useTheme } from '../../hooks'
 import MapView, { PROVIDER_GOOGLE, Marker, AnimatedRegion, Callout } from 'react-native-maps';
+import { tow_bike, tow_truck, tow_private } from '../../assets'
 
 const Body = ({ _this }) => {
     const [Colors, styles] = useTheme(style)
@@ -17,22 +18,45 @@ const Body = ({ _this }) => {
         <View style={styles.flex1}>
             <MapView
                 style={styles.flex1}
-                //ref={_this.map}
+                ref={_this.map}
                 provider={PROVIDER_GOOGLE}
                 showsUserLocation={true}
                 followsUserLocation={true}
-                onMapReady={() => null}
+                onMapReady={() => _this.requestLocationPermission()}
                 loadingEnabled={true}
-                initialRegion={{
-                    latitude: 31.766905,
-                    longitude: 35.215293,
-                    latitudeDelta: 0.005,
-                    longitudeDelta: 0.005,
-                }}
+                showsCompass={false}
+                //onUserLocationChange={_this.onUserLocationChange}
+                rotateEnabled={false}
+                //showsMyLocationButton={true}
+                initialRegion={_this.currentLocation}
             >
-                
+                {
+                    _this.nearbyTows.map(item => <MarkerRender key={item._id} item={item} />)
+                }
             </MapView>
         </View>
+    )
+}
+
+const MarkerRender = ({ item }) => {
+    const [Colors, styles] = useTheme(style)
+    const title = item.active_vehicle.type == 'BIKE' ? 'Tow Bike' : item.active_vehicle.type == 'TRUCK' ? 'Tow Truck/Bus' : 'Tow Private'
+    const icon = item.active_vehicle.type == 'BIKE' ? tow_bike : item.active_vehicle.type == 'TRUCK' ? tow_truck : tow_private
+    return (
+        <Marker
+            flat={true}
+            title={title}
+            coordinate={{ latitude: item.location.coordinates[1], longitude: item.location.coordinates[0] }}
+            style={{
+                transform: [{
+                    rotate: item.location.heading === undefined ? '0deg' : `${item.location.heading}deg`
+                }]
+            }}
+        >
+            <View style={styles.marker}>
+                <Image source={icon} style={styles.markerImage} />
+            </View>
+        </Marker>
     )
 }
 
