c8bf2e4fd4977a19f6a6af4d9d47acda2a2bb430
pashentiy <pashentiy.90@gmail.com>
2021-04-15 16:31:24 +0300

    added new styles for Home screen

--- frontend/src/screens/Home/style.js
+++ frontend/src/screens/Home/style.js
@@ -3,8 +3,12 @@ import {
 } from 'react-native';
 import { Mixins, Spacing, Typography } from '../../styles'
 
-const styles = ({Colors})=>(StyleSheet.create({
-    header:{
+const styles = ({ Colors }) => (StyleSheet.create({
+    fullHeightContainer: {
+        height: Mixins.DEVICE_HEIGHT,
+        flex: 0
+    },
+    header: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         position: 'absolute',
@@ -13,13 +17,13 @@ const styles = ({Colors})=>(StyleSheet.create({
         left: 0,
         width: '100%'
     },
-    hamburger:{
+    hamburger: {
         width: Mixins.scaleSize(40),
         height: Mixins.scaleSize(40),
         resizeMode: 'contain',
         margin: Spacing.SCALE_10
     },
-    searchContainer:{
+    searchContainer: {
         flex: 1,
         backgroundColor: Colors.white,
         height: Mixins.scaleSize(40),
@@ -34,11 +38,63 @@ const styles = ({Colors})=>(StyleSheet.create({
         shadowRadius: 3.84,
         elevation: 5,
         justifyContent: 'center',
-        padding: Spacing.SCALE_10
+        paddingHorizontal: Spacing.SCALE_10
     },
-    searchInput:{
+    searchInput: {
         flex: 1,
-        fontSize: Typography.FONT_SIZE_18
+        fontSize: Typography.FONT_SIZE_18,
+    },
+    searchResultContainer: {
+        width: Mixins.scaleSize(280),
+        marginLeft: Spacing.SCALE_10
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 0
+    },
+    modalView: {
+        width: '100%',
+        backgroundColor: Colors.background,
+        borderRadius: 5,
+        padding: Spacing.SCALE_15,
+        alignItems: 'center',
+        shadowColor: Colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    popupText: {
+        textAlign: 'center',
+        fontSize: Typography.FONT_SIZE_14,
+        color: Colors.primary
+    },
+    popupButton: {
+        backgroundColor: Colors.primary,
+        borderRadius: 10,
+        padding: Spacing.SCALE_10,
+        marginTop: Spacing.SCALE_10
+    },
+    popupButtonText: {
+        color: Colors.background,
+        fontSize: Typography.FONT_SIZE_16
+    },
+    marker: {
+        //width: 20,
+        //height: 20,
+    },
+    markerImage: {
+        width: Mixins.scaleSize(30),
+        height: Mixins.scaleSize(30),
+        resizeMode: 'contain',
+        transform: [{
+            rotate: '270deg'
+        }]
     }
 })
 );
