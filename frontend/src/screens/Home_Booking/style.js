84b14d72d076519c1621c04f780381491290e050
pashentiy <pashentiy.90@gmail.com>
2021-04-17 19:18:15 +0300

    added new styles for whole Home Booking page

--- frontend/src/screens/Home_Booking/style.js
+++ frontend/src/screens/Home_Booking/style.js
@@ -28,6 +28,69 @@ const style = ({ Colors }) => (StyleSheet.create({
         backgroundColor: Colors.background,
         paddingHorizontal: Spacing.SCALE_10
     },
+    popupTitle: {
+        fontSize: Typography.FONT_SIZE_15,
+        color: Colors.text,
+        fontWeight: 'bold',
+        marginBottom: Spacing.SCALE_5
+    },
+    towImageContainer:{
+        backgroundColor: Colors.white,
+        borderRadius: Spacing.SCALE_10,
+        padding: Spacing.SCALE_5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: Mixins.scaleSize(100)
+    },
+    towImageContainerSelected:{
+        borderWidth: 2,
+        borderColor: Colors.primary
+    },
+    towName:{
+        fontSize: Typography.FONT_SIZE_12,
+        color: Colors.muted_text
+    },
+    towImage:{
+        resizeMode: 'contain',
+        width: Mixins.scaleSize(40),
+        height: Mixins.scaleSize(40)
+    },
+    locationTitle:{
+        fontSize: Typography.FONT_SIZE_12,
+        color: Colors.muted_text
+    },
+    location:{
+        fontSize: Typography.FONT_SIZE_14,
+        color: Colors.black,
+        marginRight: Spacing.SCALE_10
+    },
+    continueButton:{
+        backgroundColor: Colors.black,
+        padding: Spacing.SCALE_8,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    continueButtonIcon:{
+        marginLeft: Spacing.SCALE_10,
+        marginRight: Spacing.SCALE_10,
+        backgroundColor: Colors.secondary20,
+        width: Mixins.scaleSize(30),
+        height: Mixins.scaleSize(30),
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    continueButtonText:{
+        fontSize: Typography.FONT_SIZE_18,
+        color: Colors.white,
+        flex: 1,
+        textAlign: 'center'
+    },
+    avatarGif:{
+        resizeMode: 'contain',
+        width: Mixins.scaleSize(150),
+        height: Mixins.scaleSize(150)
+    }
 })
 );
 
