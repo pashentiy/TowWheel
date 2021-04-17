fcd62f723dfbc8db634576dca7602d812e5f9178
pashentiy <pashentiy.90@gmail.com>
2021-04-17 19:16:18 +0300

    added logic for Home Booking + body list of drivers requests

--- frontend/src/screens/Home_Booking/index.js
+++ frontend/src/screens/Home_Booking/index.js
@@ -1,16 +1,22 @@
-import React, { useEffect, useRef } from 'react';
-import { Toast } from '../../components'
+import React, { useState, useEffect, useRef } from 'react';
+import { Container, Toast } from '../../components'
+import API from '../../services/api'
 import Geocoder from 'react-native-geocoding';
 import { useDdux } from '../../hooks'
+import Body from './body'
 import Header from './header'
 
-const Booking = ({ route }) => {
+const Booking = ({ route, navigation }) => {
   const { destination = null, source = null } = route.params
   const Ddux = useDdux()
   const rideDetails = Ddux.cache('ride')
   const userDetails = Ddux.cache('user')
   const currentLocation = source
   const map = useRef(null)
+  const [distanceTime, setDistanceTime] = useState({distance: null, duration: null})
+  const [pickupPoint, setPickupPoint] = useState(null)
+  const [towType, setTowType] = useState('BIKE')
+  const [popupStep,setPopupStep] = useState(rideDetails!=null ? 1 : 0)
 
     useEffect(()=>{
         Geocoder.from(currentLocation)
@@ -22,18 +28,30 @@ const Booking = ({ route }) => {
         })      
     },[])
 
+    const createRideRequest = async()=>{
+      /*
+       * API createRideRequest
+       */
+      Ddux.setData('loading',true)
+      let response = await API.createRideRequest({
+        source: [currentLocation.longitude,currentLocation.latitude],
+        destination: [destination.location.lng, destination.location.lat],
+        distance: distanceTime.distance, // In KM
+        time: distanceTime.duration, // In minute,
+        tow_type: towType
+      })
       Ddux.setData('loading',false)
       if (!response.status) {
         return Toast.show({ type: 'error', message: response.error })
       }
       Ddux.setCache('ride',{...response.data,source: source, destination: destination, tow_type: towType, popupStep: 1})
       setPopupStep(1)
-  
+    }
 
   return (
     <Container isTransparentStatusBar={false}>
       <Header _this={{ navigation }} />
-      {/* TO DO : // Body */}
+      <Body _this={{ navigation, destination, map, currentLocation, setDistanceTime, pickupPoint,towType, setTowType, popupStep, setPopupStep, createRideRequest }} />
     </Container>
   )
 }
