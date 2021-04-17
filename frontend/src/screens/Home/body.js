0fe16751c42c2a9a43e794ec2ffd4abf2f830297
pashentiy <pashentiy.90@gmail.com>
2021-04-17 19:21:51 +0300

    cleaning unused imports

--- frontend/src/screens/Home/body.js
+++ frontend/src/screens/Home/body.js
@@ -1,13 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import {
     View,
-    Text,
-    TouchableOpacity,
     Image
 } from 'react-native';
 import style from './style'
-import { Mixins } from '../../styles'
-import Icon from 'react-native-vector-icons/FontAwesome';
 import { useTheme } from '../../hooks'
 import MapView, { PROVIDER_GOOGLE, Marker, AnimatedRegion, Callout } from 'react-native-maps';
 import { tow_bike, tow_truck, tow_private } from '../../assets'
