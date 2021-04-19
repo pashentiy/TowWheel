e9653365b4cafef8b2eacc938e5faf2a10b3e984
pashentiy <pashentiy.90@gmail.com>
2021-04-20 02:32:19 +0300

    added API getMyRideRequest

--- frontend/src/services/api/home.js
+++ frontend/src/services/api/home.js
@@ -19,6 +19,21 @@ const home = {
         }
         return handleResponse(response);
     },
+    getMyRideRequest: async () => {
+        let response = null
+        const token = await getToken()
+        try {
+            response = await API.get(`/home/get-my-ride-request`,
+                {
+                    'headers': { 'Authorization': 'Bearer: ' + token }
+                }
+            )
+        }
+        catch (e) {
+            response = e
+        }
+        return handleResponse(response);
+    },
     createRideRequest: async (data) => {
         let response = null
         const token = await getToken()
