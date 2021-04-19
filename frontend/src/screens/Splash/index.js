e101075590bf4fa55feef72bed0119fb597b4509
pashentiy <pashentiy.90@gmail.com>
2021-04-20 00:24:48 +0300

    adedd logic for authentication of user while user's enter to the app by it's token

--- frontend/src/screens/Splash/index.js
+++ frontend/src/screens/Splash/index.js
@@ -6,14 +6,54 @@ import {
 } from 'react-native';
 import style from './style'
 import { logo } from '../../assets'
-import { Container } from '../../components'
-import { useTheme } from '../../hooks'
+import Config from '../../config'
+import { useDdux, useTheme } from '../../hooks'
+import API from '../../services/api'
+import { Container, Toast } from '../../components'
+
+var initialLoading = false
 
 const Splash = ({ navigation }) => {
     const [Colors, styles] = useTheme(style)
+    const Ddux = useDdux()
+    const userDetails = Ddux.cache('user')
+
+    useEffect(()=>{
+        initialLoading = false
+    },[])
+
     useEffect(() => {
-        setTimeout(()=>navigation.replace('Home'),2000)
-    }, [])
+        if (userDetails !== null && !initialLoading){
+            initialLoading = true 
+            verifyLoginSession()
+        }
+    }, [userDetails])
+
+    const verifyLoginSession = async () => {
+        if (Object.keys(userDetails).length == 0)
+            return navigation.replace('Home')
+        try {
+            /*
+             * API LoginByToken
+             */
+            let response = await API.loginByToken(userDetails.mobile, userDetails.active_session_refresh_token)
+            if (!response.status) {
+                return Toast.show({ type: 'error', message: response.error })
+            }
+            else if (!response.data.isUserExists) {
+                Ddux.setCache('user', {})
+            }
+            else {
+                response.data.token_expiry = new Date().getTime() + 45 * 60000;
+                Config.session = { mobile: response.data.mobile, active_session_refresh_token: response.data.active_session_refresh_token, access_token: response.data.access_token, token_expiry: response.data.token_expiry }
+                Ddux.setCache('user', response.data)
+            }
+            navigation.replace('Home')
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
     return (
         <Container style={styles.centerAll} isTransparentStatusBar={false}>
             <View style={styles.flex1, styles.centerAll}>
