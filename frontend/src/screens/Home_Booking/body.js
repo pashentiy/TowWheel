752e7905f54d4c3669a1fe4cb4713721b0b2e238
pashentiy <pashentiy.90@gmail.com>
2021-04-20 02:05:22 +0300

    new settings for map & marker locations

--- frontend/src/screens/Home_Booking/body.js
+++ frontend/src/screens/Home_Booking/body.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { View } from 'react-native';
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    TextInput
+} from 'react-native';
 import { useTheme } from '../../hooks'
 import style from './style'
+import { Mixins } from '../../styles'
 import { GOOGLE_MAP_API_KEY } from '../../config'
 import Icon from 'react-native-vector-icons/Ionicons';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, AnimatedRegion, Callout } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions'
 import BottomPopup from './bottomPopup'
 
@@ -27,25 +34,25 @@ const Body = ({ _this }) => {
                 //onUserLocationChange={_this.onUserLocationChange}
                 rotateEnabled={false}
                 //showsMyLocationButton={true}
-                initialRegion={_this.currentLocation}
+                initialRegion={{..._this.source,latitudeDelta: 0.02,longitudeDelta: 0.02,}}
             >
                 <Marker
                     identifier='source'
-                    coordinate={{ latitude: _this.currentLocation.latitude, longitude: _this.currentLocation.longitude }}
+                    coordinate={{ latitude: _this.source.latitude, longitude: _this.source.longitude }}
                     title={'Pick up point'}
                 >
                     <Icon name='ios-location-sharp' size={40} color={Colors.ascent} style={{ alignSelf: 'baseline' }} />
                 </Marker>
                 <Marker
                     identifier='destination'
-                    coordinate={{ latitude: _this.destination.location.lat, longitude: _this.destination.location.lng }}
+                    coordinate={{ latitude: _this.destination.latitude, longitude: _this.destination.longitude }}
                     title={'Drop off point'}
                 >
                     <Icon name='ios-location-sharp' size={40} color={Colors.primary} style={{ alignSelf: 'baseline' }} />
                 </Marker>
                 <MapViewDirections
-                    origin={{ latitude: _this.currentLocation.latitude, longitude: _this.currentLocation.longitude }}
-                    destination={{ latitude: _this.destination.location.lat, longitude: _this.destination.location.lng }}
+                    origin={{ latitude: _this.source.latitude, longitude: _this.source.longitude }}
+                    destination={{ latitude: _this.destination.latitude, longitude: _this.destination.longitude }}
                     apikey={GOOGLE_MAP_API_KEY} 
                     strokeWidth={4}
                     strokeColor={Colors.primary}
