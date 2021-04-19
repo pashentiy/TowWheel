35a2769fff261cf3222d5b69963c4d9ea37c7184
pashentiy <pashentiy.90@gmail.com>
2021-04-20 00:10:29 +0300

    added ride request and render item for FlatList

--- frontend/src/screens/Home_Driver/body.js
+++ frontend/src/screens/Home_Driver/body.js
@@ -5,16 +5,30 @@ import {
     TouchableOpacity,
     Image,
     FlatList,
+    TouchableWithoutFeedback,
+    TouchableNativeFeedback
 } from 'react-native';
 import style from './style'
 import { Mixins, Typography } from '../../styles'
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useTheme } from '../../hooks'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, AnimatedRegion, Callout } from 'react-native-maps';
 import { tow_bike, tow_truck, tow_private, avatarGif } from '../../assets'
 
 const Body = ({ _this }) => {
     const [Colors, styles] = useTheme(style)
+
+    const isRideAccepted = _this.selectedRideRequest && _this.selectedRideRequest.available_drivers.includes(_this.userDetails.driver_details) ? true : false
+
+    const renderItem = ({ item, index }) => {
+        return <RideRequest _this={_this} item={item} />
+    }
+
+    const renderSeperator = () => {
+        return (
+            <View style={{ height: Mixins.scaleHeight(10) }} />
+        )
+    }
 
     return (
         <>
@@ -41,7 +55,9 @@ const Body = ({ _this }) => {
                         <FlatList
                             contentContainerStyle={styles.flatlist}
                             data={_this.rideRequests}
+                            renderItem={renderItem}
                             style={[styles.fullWidth]}
+                            ItemSeparatorComponent={renderSeperator}
                             keyExtractor={(item, index) => index.toString()}
                             keyboardShouldPersistTaps='handled'
                             onEndReached={() => null}
@@ -54,7 +70,7 @@ const Body = ({ _this }) => {
                         />
                         {_this.selectedRideRequest && 
                         <TouchableOpacity onPress={() => _this.processTowRequest()} style={[styles.marginBottom10, styles.flexRow, styles.continueButton]}>
-                            <Text> 'Decline Tow Request or Accept Tow Request' </Text>
+                            <Text style={styles.continueButtonText}>{isRideAccepted ? 'Decline Tow Request' : 'Accept Tow Request'}</Text>
                             <View style={styles.continueButtonIcon}>
                                 <Icon name='ios-arrow-forward-sharp' size={Typography.FONT_SIZE_25} color={Colors.primary} />
                             </View>
@@ -69,6 +85,44 @@ const Body = ({ _this }) => {
                 </>}
             </View>
         </>
+    )
+}
+
+
+const RideRequest = ({ _this, item }) => {
+    const [Colors, styles] = useTheme(style)
+    const icon = item.required_vehicle_type == 'BIKE' ? tow_bike : item.required_vehicle_type == 'TRUCK' ? tow_truck : tow_private
+    return (
+        <TouchableOpacity onPress={() => _this.setSelectedRideRequest(item)}>
+            <View style={[styles.paddingHorizontal10, styles.paddingVertical10, (_this.selectedRideRequest && item._id == _this.selectedRideRequest._id) ? styles.selected : null]}>
+                <View style={[styles.flexRow, styles.flex1, styles.centerAll]}>
+                    <View style={styles.centerAll}>
+                        <Image source={icon} style={styles.towImage} />
+                        <Text style={styles.vehicleName}>{item.required_vehicle_type}</Text>
+                    </View>
+                    <View style={[styles.flex1, styles.paddingLeft10]}>
+                        <View style={[styles.flexRow, styles.alignCenter, styles.marginBottom5]}>
+                            <Icon name='ios-location-sharp' size={Mixins.scaleSize(20)} color={Colors.ascent} />
+                            <View>
+                                <Text style={styles.locationTitle}>Pickup</Text>
+                                <Text numberOfLines={1} style={styles.location}>{item.source.address}</Text>
+                            </View>
+                        </View>
+                        <View style={[styles.flexRow, styles.alignCenter]}>
+                            <Icon name='ios-location-sharp' size={Mixins.scaleSize(20)} color={Colors.primary} />
+                            <View>
+                                <Text style={styles.locationTitle}>Drop-off</Text>
+                                <Text numberOfLines={1} style={styles.location}>{item.destination.address}</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.centerAll}>
+                        <Text style={styles.value}>{parseFloat(item.distance).toFixed(1)}<Text style={styles.symbol}> km</Text></Text>
+                        <Text style={styles.value}><Text style={styles.symbol}>$ </Text>{parseFloat(220.00).toFixed(2)}</Text>
+                    </View>
+                </View>
+            </View>
+        </TouchableOpacity>
     )
 }
 
