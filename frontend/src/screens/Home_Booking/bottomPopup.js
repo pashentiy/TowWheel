5a31c7d61bb2fc94d3197cfb3089dbb9777c87a8
pashentiy <pashentiy.90@gmail.com>
2021-04-20 02:13:39 +0300

    added TowSearchProgress will show us FlatList with all avaliable drivers that have sended request, to users with details

--- frontend/src/screens/Home_Booking/bottomPopup.js
+++ frontend/src/screens/Home_Booking/bottomPopup.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import {
     View,
     Text,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    TextInput,
+    FlatList
 } from 'react-native';
 import { useTheme } from '../../hooks'
 import style from './style'
 import { Mixins, Typography } from '../../styles'
+import Config, { API_STORAGE } from '../../config'
 import Icon from 'react-native-vector-icons/Ionicons';
+import Icon2 from 'react-native-vector-icons/FontAwesome';
 import { tow_bike, tow_truck, tow_private, avatarGif } from '../../assets'
 
 
@@ -28,14 +32,73 @@ const BottomPopup = ({ _this }) => {
 
 const TowSearchProgress = ({ _this }) => {
     const [Colors, styles] = useTheme(style)
-    return (
+
+    const renderItem = ({ item, index }) => {
+        let rating = 0
+        for(let i = 0; i<item.reviews.length; i++){
+            rating += item.reviews[i].rating
+        }
+        rating = parseFloat(rating / item.reviews.length || 0).toFixed(1)
+        return (
+            <View style={styles.renderItem}>
+                <Image source={{ uri: API_STORAGE + item.profile_picture }} style={styles.dp} />
+                <Text style={styles.itemName}>{item.user_details.name}</Text>
+                <Text style={styles.cost}><Text style={styles.currency}>$</Text> {parseFloat(item.vehicle_details.cost_per_km * _this.rideDetails.distance).toFixed(2)}</Text>
+                <View style={styles.rating}>
+                    <Icon2 name='star' size={Typography.FONT_SIZE_16} color={Colors.primary} />
+                    <Text style={styles.ratingValue}> {rating}</Text>
+                </View>
+            </View>
+        )
+    }
+
+
+    return _this.rideDetails && (
         <View style={styles.content}>
-            <View style={[styles.flex1,styles.centerAll]}>
+
+            <View style={[styles.flexRow, styles.alignCenter, styles.spaceBetween, styles.headerDistanceTime]}>
+                <View style={[styles.flexRow, styles.alignCenter]}>
+                    <Icon name='location' size={Typography.FONT_SIZE_22} color={Colors.black} />
+                    <Text style={styles.distance}>{parseFloat(_this.rideDetails.distance).toFixed(1)} km</Text>
+                </View>
+                <View style={[styles.flexRow, styles.alignCenter]}>
+                    <Icon name='time' size={Typography.FONT_SIZE_22} color={Colors.black} />
+                    <Text style={styles.distance}>{parseFloat(_this.rideDetails.time / 60).toFixed(1)} hr</Text>
+                </View>
+                <View style={[styles.flexRow, styles.alignCenter]}>
+                    <Icon2 name='toggle-on' size={Typography.FONT_SIZE_25} color={Colors.primary} />
+                    <Text style={styles.distance}> Popularity</Text>
+                </View>
+            </View>
+
+            {_this.rideDetails.available_drivers.length > 0 &&
+                <>
+                    <FlatList
+                        //contentContainerStyle={styles.flatlist}
+                        data={_this.rideDetails.available_drivers}
+                        renderItem={renderItem}
+                        style={[styles.fullWidth]}
+                        ItemSeparatorComponent={null}
+                        keyExtractor={(item, index) => index.toString()}
+                        keyboardShouldPersistTaps='handled'
+                        onEndReached={() => null}
+                        refreshing={false}
+                        onRefresh={() => null}
+                        horizontal={true}
+                        showsVerticalScrollIndicator={false}
+                        ListFooterComponent={null}
+                        ListHeaderComponent={null}
+                    />
+                </>
+            }
+
+            {_this.rideDetails.available_drivers.length < 1 && <View style={[styles.flex1, styles.centerAll]}>
                 <Image source={avatarGif} style={styles.avatarGif} />
                 <Text style={styles.popupTitle}>Connecting to Tow Drivers ..... </Text>
-            </View>
-            <TouchableOpacity onPress={()=>_this.setPopupStep(1)} style={[styles.marginBottom10, styles.flexRow, styles.continueButton]}>
-                <Text style={styles.continueButtonText}>Cancel</Text>
+            </View>}
+
+            <TouchableOpacity onPress={() => _this.cancelRideRequest()} style={[styles.marginBottom10, styles.flexRow, styles.continueButton]}>
+                <Text style={styles.continueButtonText}>Cancel Request</Text>
                 <View style={styles.continueButtonIcon}>
                     <Icon name='close' size={Typography.FONT_SIZE_25} color={Colors.primary} />
                 </View>
@@ -57,13 +120,13 @@ const TowSelector = ({ _this }) => {
                     <Image source={tow_bike} style={styles.towImage} />
                     <Text style={styles.towName}>Tow Bike</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => _this.setTowType('PRIVATE')} style={[styles.towImageContainer, (_this.towType == 'PRIVATE') ? styles.towImageContainerSelected : null]}>
-                    <Image source={tow_private} style={styles.towImage} />
-                    <Text style={styles.towName}>Tow Private</Text>
-                </TouchableOpacity>
                 <TouchableOpacity onPress={() => _this.setTowType('TRUCK')} style={[styles.towImageContainer, (_this.towType == 'TRUCK') ? styles.towImageContainerSelected : null]}>
                     <Image source={tow_truck} style={styles.towImage} />
                     <Text style={styles.towName}>Tow Truck/Bus</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => _this.setTowType('PRIVATE')} style={[styles.towImageContainer, (_this.towType == 'PRIVATE') ? styles.towImageContainerSelected : null]}>
+                    <Image source={tow_private} style={styles.towImage} />
+                    <Text style={styles.towName}>Tow Private</Text>
                 </TouchableOpacity>
             </View>
             <View style={[styles.flex1, styles.marginTop10]}>
@@ -71,7 +134,7 @@ const TowSelector = ({ _this }) => {
                     <Icon name='ios-location-sharp' size={Mixins.scaleSize(25)} color={Colors.ascent} />
                     <View>
                         <Text style={styles.locationTitle}>Pickup</Text>
-                        <Text numberOfLines={1} style={styles.location}>{_this.pickupPoint}</Text>
+                        <Text numberOfLines={1} style={styles.location}>{_this.source.address}</Text>
                     </View>
                 </View>
                 <View style={[styles.flexRow, styles.alignCenter]}>
@@ -82,7 +145,7 @@ const TowSelector = ({ _this }) => {
                     </View>
                 </View>
             </View>
-            <TouchableOpacity onPress={()=>_this.createRideRequest()} style={[styles.marginBottom10, styles.flexRow, styles.continueButton]}>
+            <TouchableOpacity onPress={() => _this.createRideRequest()} style={[styles.marginBottom10, styles.flexRow, styles.continueButton]}>
                 <Text style={styles.continueButtonText}>Continue</Text>
                 <View style={styles.continueButtonIcon}>
                     <Icon name='ios-arrow-forward-sharp' size={Typography.FONT_SIZE_25} color={Colors.primary} />
