970df485e84ca80fd415ff49f4bc2c6e754e5104
pashentiy <pashentiy.90@gmail.com>
2021-04-20 00:30:29 +0300

    added new api call for home

--- frontend/src/services/api/home.js
+++ frontend/src/services/api/home.js
@@ -1,12 +1,17 @@
 import { handleResponse, API, MOCK, getToken } from './utils';
 
 const home = {
-    getRecentlyActiveUsers: async () => {
+    getNearestTows: async (latitude, longitude) => {
         let response = null
-        const token = await getToken()
         try {
-            response = await API.get(`/home/get-recent-active-users`,
-                { 'headers': { 'Authorization': 'Bearer: ' + token } }
+            response = await API.get(`/home/get-nearest-tows`,
+                {
+                    params: {
+                        latitude: latitude,
+                        longitude: longitude
+                    },
+                    //'headers': { 'Authorization': 'Bearer: ' + token }
+                }
             )
         }
         catch (e) {
@@ -14,36 +19,22 @@ const home = {
         }
         return handleResponse(response);
     },
-    getIntroductions: async (page=1) => {
+    createRideRequest: async (data) => {
         let response = null
         const token = await getToken()
         try {
-            response = await API.get(`/home/get-newsfeeds/?page=${page}`,
-                { 'headers': { 'Authorization': 'Bearer: ' + token } }
-            )
-        }
-        catch (e) {
-            response = e
-        }
-        
-        return handleResponse(response);
-    },
-    sendLike: async (id, isLiked) => {
-        let response = null
-        const token = await getToken()
-        try {
-            response = await API.post('/home/send-like', {
-                profile_picture_id: id,
-                isLiked: isLiked
-            },
-                { 'headers': { 'Authorization': 'Bearer: ' + token } }
+            response = await API.post(`/home/create-ride-request`,
+                data,
+                {
+                    'headers': { 'Authorization': 'Bearer: ' + token }
+                }
             )
         }
         catch (e) {
             response = e
         }
         return handleResponse(response);
-    },
+    }
 }
 
 export default home
