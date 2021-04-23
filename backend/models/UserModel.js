943f654f3b35db16ed6b7b54856fae56c3b3d19c
pashentiy <pashentiy.90@gmail.com>
2021-04-23 20:07:31 +0300

    fixed bug - added default to driver details Schema

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema({
 	is_driver: { type: Boolean, required: true, default: false },
 	active_session_refresh_token: { type: String, required: true },
 	access_token: { type: String },
-    driver_details: { type: Schema.Types.ObjectId, ref: 'drivers' },
+    driver_details: { type: Schema.Types.ObjectId, ref: 'drivers', default: null },
 },{ timestamps: true })
 
 const UserModel = mongoose.model('users', UserSchema)
