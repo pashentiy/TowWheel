2a9aabdcd54cc64cb1dde574f22efe8c8e5d0e80
pashentiy <pashentiy.90@gmail.com>
2021-04-23 20:05:42 +0300

    Driver controller implementation

---
+++ backend/controllers/DriverController.js
@@ -0,0 +1,64 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Config = require('../config.js');
+const fs = require('fs');
+const { User, ProfilePicture, Driver, Vehicle, Ride, Mongoose } = require('../models')
+
+const {
+	IsExists, IsExistsOne, Insert, Find, FindOne, CompressImageAndUpload, FindAndUpdate, Delete,
+	HandleSuccess, HandleError, HandleServerError,
+	ValidateEmail, PasswordStrength, ValidateAlphanumeric, ValidateLength, ValidateMobile, isDataURL, GeneratePassword, Aggregate
+} = require('./BaseController');
+
+module.exports = {
+
+	getNearestRideRequest: async (req, res, next) => {
+		try {
+			let { latitude, longitude } = req.query
+            const user_id = req.user_id
+            const driver_data = await FindOne({
+                model: User,
+                where: { _id: user_id},
+                select: { driver_details: 1 }
+            })
+			if(!driver_data)
+				return HandleError(res, 'Driver not found.')
+			const driver_id = driver_data.driver_details
+            
+			const driver_vehicles_data = await Find({
+                model: Driver,
+                where: { _id: driver_id},
+                select: { vehicles: 1 },
+				populate: 'vehicles'
+            })
+			if(!driver_vehicles_data)
+				return HandleError(res, 'Driver details not found.')
+			const driver_owned_vehicles = driver_vehicles_data[0].vehicles.map(item=>item.type)
+
+			const data = await Find({
+				model: Ride,
+				where: {
+					source: {
+						$near: {
+							$geometry: {
+								type: "Point",
+								coordinates: [longitude,latitude]
+							},
+							$maxDistance: Config.max_map_range,
+						}
+					},
+					ride_status: 'searching',
+					required_vehicle_type: { $in: driver_owned_vehicles }
+				}
+			})
+
+			if (!data)
+				return HandleError(res, 'Failed to fetch data.')
+
+			return HandleSuccess(res, data)
+
+		} catch (err) {
+			HandleServerError(res, req, err)
+		}
+	}
+}
--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,3 +1,5 @@
+
 exports.Auth = require('./AuthController');
 exports.Socket = require('./SocketController');
 exports.Home = require('./HomeController');
+exports.Driver = require('./DriverController');
