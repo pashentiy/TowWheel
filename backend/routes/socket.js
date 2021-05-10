0bc596d00f268bca63f412b19f2d152873ce3bed
pashentiy <pashentiy.90@gmail.com>
2021-05-10 21:28:15 +0300

    added namespace LiveChat (communication channel  that allows you to split the logic of your application)

--- backend/routes/socket.js
+++ backend/routes/socket.js
@@ -12,5 +12,6 @@ module.exports = (io) => {
     io.of('/user-ride-request').on('connection',(socket)=>Socket.UserRideRequest(socket,io));
     io.of('/user-driver-inprogress').on('connection',(socket)=>Socket.UserDriverInprogress(socket,io));
     io.of('/realtime-database-updates').on('connection',(socket)=>Socket.RealtimeDatabaseUpdates(socket,io));
+    io.of('/live-chat').on('connection',(socket)=>Socket.LiveChat(socket,io));
 };
 
