1ca71f47cc80b10a5e12673f8c4cc1f721570199
pashentiy <pashentiy.90@gmail.com>
2021-05-12 17:19:25 +0300

    added useEffect mount socketHandler() adedd useEffect unmount socket.close() added socketHandler() - added listener connect   - sending emit custom event initialize_chat - added listener new_message   - sending emit custom event message_seen - added listener bulk_seen   - mark all messages as seen with double tick - added linstener message_seen   - for a single message mark as seen during conversation - added listener disconnect
    added sendMessage
    - sending emit of a new message

--- frontend/src/screens/Chat/index.js
+++ frontend/src/screens/Chat/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-
-import { Container } from '../../components'
+import React, { useState, useEffect, useRef, useReducer } from 'react';
+import {
+  View,
+  Text,
+  BackHandler,
+  Platform,
+  Linking
+} from 'react-native';
+import styles from './style'
+import Config from '../../config'
+import { useIsFocused } from '@react-navigation/native';
+import { Container, Toast } from '../../components'
+import { Mixins, Spacing, Typography } from '../../styles'
+import API from '../../services/api'
 import { useDdux } from '../../hooks'
 import Body from './body'
 import Header from './header'
@@ -8,16 +19,76 @@ import Header from './header'
 var socket = null
 
 const Chat = ({ route, navigation }) => {
+  const { name, partner_id, ride_id } = route.params
+  const isFocused = useIsFocused()
   const Ddux = useDdux()
+  const userDetails = Ddux.cache('user')
+
+  const [chatData,setChatData] = useState([])
+
+  useEffect(()=>{
+    socketHandler()
+    return () => {
+      if (socket)
+        socket.close();
+    }
+  },[])
+
 
   /*
-   * TODO:// Socket Handler
+   * Socket Handler
    */
+  const socketHandler = async () => {
+    socket = await API.SOCKET('/live-chat')
+    socket.on('connect', () => {
+      socket.emit('initialize_chat', { user_id: userDetails._id, partner_id: partner_id, ride_id: ride_id }, (response) => {
+        setChatData(prev => response.reverse())
+      })
+    });
+
+    socket.on('new_message', (data) => {
+      setChatData(prev => {
+        return [data,...prev]
+      })
+      socket.emit('message_seen',data._id)
+    });
+
+    socket.on('bulk_seen', (data) => {
+      setChatData(prev => data.reverse())
+    });
+
+
+    socket.on('message_seen', (message_id) => {
+      setChatData(prev => {
+        const temp = prev.map(item=>{
+          if(item._id == message_id){
+            item.seen = true
+          }
+          return item
+        })
+        return temp
+      })
+    });
+
+    socket.on('disconnect', () => {
+
+    });
+  }
+
+  const sendMessage = async (message,setMessage)=>{
+    setMessage('')
+    socket.emit('new_message', message, response=>{
+      setChatData(prev => {
+        return [response,...prev]
+      })
+    })
+  }
+  
   
   return (
     <Container isTransparentStatusBar={false}>
-      <Header _this={{ }} />
-      <Body _this={{ }} />
+      <Header _this={{ navigation, name }} />
+      <Body _this={{ navigation, userDetails, chatData, sendMessage }} />
     </Container>
   )
 }
