7f225f4db45d1cc49ef18394d13d1b05c9dfd4ba
pashentiy <pashentiy.90@gmail.com>
2021-07-06 17:51:18 +0300

    added switch between 2 modes driver mode & user mode

--- frontend/src/navigation/Drawer.js
+++ frontend/src/navigation/Drawer.js
@@ -33,7 +33,7 @@ const Dashboard = () => {
             drawerContent={(props) => <CustomDrawerContent {...props} />}
         >
             <Drawer.Screen name="Home" options={{ headerShown : false }} component={userDetails && userDetails.is_driver ? Screen.Home_Driver : Screen.Home} />
-            <Drawer.Screen name="Notifications" options={{ headerShown : false }} component={Screen.Home} />
+            {userDetails && userDetails.is_driver && <Drawer.Screen name="User Mode" options={{ headerShown : false }} component={Screen.Home} />}
         </Drawer.Navigator>
         </>
     );
