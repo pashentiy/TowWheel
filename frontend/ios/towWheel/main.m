cb63ebb945d675d76ac79a6a5ab4f6f5d398d3b3
pashentiy <pashentiy.90@gmail.com>
2021-07-19 00:04:04 +0300

    Detox Settings

--- frontend/ios/towWheel/main.m
+++ frontend/ios/towWheel/main.m
@@ -3,6 +3,14 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+  Cycle in dependencies between targets 'React-RCTText' and 'React'; building could produce unreliable results.
+  Cycle path: React-RCTText  react-native-geolocation-service  React  React-RCTText
+  Cycle details:
+  Target 'React-RCTText': Libtool /Users/pavel/Library/Developer/Xcode/DerivedData/TowWheel-gqozmbzdsmanvxdvnbydimjxhoam/Build/Products/Debug-iphonesimulator/React-RCTText/libReact-RCTText.a normal
+  Target 'React-RCTText' has compile command with input '/Users/pavel/Desktop/TowWheel/towheel/frontend/node_modules/react-native/Libraries/Text/VirtualText/RCTVirtualTextViewManager.m'
+  Target 'react-native-geolocation-service'
+  Target 'React' has target dependency on Target 'React-RCTText'
+  Target 'React-RCTText' has compile command with input '/Users/pavel/Desktop/TowWheel/towheel/frontend/node_modules/react-native/Libraries/Text/Text/NSTextStorage+FontScaling.m'
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
